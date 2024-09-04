81e9063615ea8443cc0e4a0dca83c4d08f041b98
Adam Cassis <adam.cassis@automattic.com>
2024-09-04 13:31:08 +0200

    feat: adjust author bio pattern

--- patterns/author-bio/author-bio-avatar.php
+++ patterns/author-bio/author-bio-avatar.php
@@ -17,16 +17,16 @@
 	<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
 	<!-- /wp:separator -->
 
-	<!-- wp:group {"metadata":{"name":"<?php esc_html_e( 'Content', 'newspack-block-theme' ); ?>"},"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
+	<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 	<div class="wp-block-group">
+		<!-- wp:avatar {"size":128,"align":"right","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|20","right":"0","left":"0"}}}} /-->
 
-		<!-- wp:avatar {"size":128,"align":"right","style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|20","right":"0","left":"var:preset|spacing|40"}}}} /-->
-
-		<!-- wp:post-author-name {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"contrast","fontSize":"large"} /-->
-
-		<!-- wp:post-author-biography /-->
-
-	</div>
+		<!-- wp:group {"style":{"layout":{"selfStretch":"fit","flexSize":null},"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-group">
+			<!-- wp:post-author-name {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}},"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"contrast","fontSize":"large"} /-->
+			<!-- wp:post-author-biography /--></div>
+		<!-- /wp:group -->
+		</div>
 	<!-- /wp:group -->
 
 	<!-- wp:separator {"className":"is-style-wide"} -->
